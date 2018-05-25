7367427e96afdfde43f2b9428393a3e380b80189
pitian <1023041650@qq.com>
2018-05-25 11:04:14 +0800

    new project

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -0,0 +1,21 @@
+package com.taotao.service.impl;
+
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 商品管理Service
+ */
+@Service
+public class ItemServiceImpl implements ItemService {
+    @Autowired
+    private TbItemMapper tbItemMapper;
+    @Override
+    public TbItem getItemById(long itemId) throws Exception {
+        TbItem tbItem = tbItemMapper.selectByPrimaryKey(itemId);
+        return tbItem;
+    }
+}
