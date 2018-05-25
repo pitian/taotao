088e45d5baa5178575365d4680b4263188c6dee9
pitian <1023041650@qq.com>
2018-05-25 16:53:04 +0800

    远程调用，pojo实现序列化

--- taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -4,6 +4,7 @@ import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,7 +15,7 @@ public class ItemController {
 
     @RequestMapping("/item/{itemId}")
     @ResponseBody
-    public TbItem getItemById(long itemId)throws  Exception{
+    public TbItem getItemById(@PathVariable long itemId)throws  Exception{
         TbItem tbItem = itemService.getItemById(itemId);
         return tbItem;
     }
